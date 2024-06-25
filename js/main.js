e442b415f149be6bd161c4133f33a257943639b5
Dzarel Developer <112155072+DzarelDeveloper@users.noreply.github.com>
2024-06-25 15:33:46 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,411 @@
+xgallery_images = [
+    {
+      url: "images/Gallery-1.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-2.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-3.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-4.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-5.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-6.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-7.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-8.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-9.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-10.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-11.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-12.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-13.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-14.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-15.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-16.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-17.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-18.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-19.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-20.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-21.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-22.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-23.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-24.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-25.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-26.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-27.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-28.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-29.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-30.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-31.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-32.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-33.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-34.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-35.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-36.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-37.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-38.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-39.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-40.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-41.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-42.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-43.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-44.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-45.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-46.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-47.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-48.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-49.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-50.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-51.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-52.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-53.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-54.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-55.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-56.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-57.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-58.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-59.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-60.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-61.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-62.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-63.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-64.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-65.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-66.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-67.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-68.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-69.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-70.jpg",
+      date: "#Smpn2Tarumajaya"
+},
+    {
+      url: "images/Gallery-71.jpg",
+      date: "#Smpn2Tarumajaya"
+},
+    {
+      url: "images/Gallery-72.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-73.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-74.jpg",
+      date: "#Smpn2Tarumajaya"
+    },
+    {
+      url: "images/Gallery-75.jpg",
+      date: "#Smpn2Tarumajaya"
+    }
+];
+  
+  
+$(document).ready(function(){
+    // SHUFFLE THE GALLERY
+    shuffle(xgallery_images);
+
+    // LOAD IMAGES INTO GALLERY
+    xgallery_images.forEach(function(xg_image, i){
+        $(".xg-container").append(createXGImage(xg_image, i));
+    });
+
+    // HOVER GALLERY IMAGE
+    $(".xg-img-wrap").mouseenter(function(){
+        var el = $(this);
+        var to = setTimeout(function(){
+            el.find(".xg-img-info").addClass("xg-img-info-open");
+        }, 100);
+        el.mouseleave(function(){
+            clearInterval(to);
+            el.find(".xg-img-info").removeClass("xg-img-info-open");
+        });
+    });
+
+
+    // OPEN IMAGE PREVIEW
+    $(".xg-img-wrap").on("click", function(){
+        var id = $(this).data("index");
+
+        $("body").addClass("scroll-fixed");
+
+        $(".xg-img-preview").fadeIn(100);
+        $(".xg-img-preview").html(createXGImagePreview(xgallery_images[id]));
+    });
+
+
+    // ZOOM IN IMAGE PREVIEW
+    $(document).on("click", ".xgp-img", function(){
+        $(".xgp-close").toggleClass("x-cloak");
+        $(this).closest(".xgp-img-inner").siblings(".xgp-details").fadeToggle(100);
+        $(this).closest(".xgp-img-inner").toggleClass("xgp-zoomed-inner");
+        $(this).toggleClass("xgp-zoomed");
+        $(".xgp-wrap").toggleClass("xg-overflow");
+    });
+
+
+    // CLOSE PREVIEW - ESCAPE
+    $(document).keyup(function(e) {
+        if (e.key === "Escape") {
+            $("body").removeClass("scroll-fixed");
+
+            $(".xg-img-preview").fadeOut(100);
+        }
+    });
+
+
+
+    // CLOSE PREVIEW - CLICK
+    $(document).on("click", ".xgp-close", function(e) {
+        $("body").removeClass("scroll-fixed");
+
+        $(".xg-img-preview").fadeOut(100);
+    });
+});
+
+
+
+function createXGImagePreview(xgi){
+    var xg_img =
+        '<div class="xgp-wrap">\
+  <div class="xgp-close"></div>\
+  <div class="xgp-img-inner">\
+  <img draggable="false" class="xgp-img" src="'+xgi.url+'">\
+  </div>\
+  <div class="xgp-details">\
+  <!--<div class="xgp-prompt">'+xgi.prompt+'</div>-->\
+  <div class="xgp-date">'+xgi.date+'</div>\
+  </div>';
+
+    return xg_img;
+}
+
+function createXGImage(xgi, i){
+    var xg_img =
+        '<div class="xg-img-wrap" data-index="'+i+'">\
+  <div class="xg-img-info">\
+  <div class="xg-img-info-inner">\
+  <!--<div class="xg-img-prompt">'+xgi.prompt+'</div>-->\
+  <div class="xg-img-date">'+xgi.date+'</div>\
+  </div>\
+  </div>\
+  <img draggable="false" class="xg-img" src="'+xgi.url+'">\
+  </div>';
+
+    return xg_img;
+}
+
+
+function shuffle(arr) {
+    let index = arr.length,  randomIndex;
+    while (index != 0) {
+        randomIndex = Math.floor(Math.random() * index);
+        index--;
+
+        [arr[index], arr[randomIndex]] = [
+            arr[randomIndex], arr[index]];
+    }
+
+    return arr;
+        }
---
+++ js/music-list.js
@@ -0,0 +1,641 @@
+
+let allMusic = [
+
+  {
+
+    name: "Beautiful in white",
+
+    artist: "shane filan",
+
+    img: "music-0",
+
+    src: "music-0"
+
+  },
+  
+  {
+
+    name: "Old Love",
+
+    artist: "yuji,putri dahlia",
+
+    img: "music-1",
+
+    src: "music-1"
+
+  },
+
+  {
+
+    name: "Eyes Blue X Terpikat Senyummu",
+
+    artist: "Stromxx",
+
+    img: "music-2",
+
+    src: "music-2"
+
+  },
+
+  {
+
+    name: "Losing Us",
+
+    artist: "Raissa Anggiani",
+
+    img: "music-3",
+
+    src: "music-3"
+
+  },
+
+  {
+
+    name: "Locked out of Heaven",
+
+    artist: "Bruno Mars",
+
+    img: "music-4",
+
+    src: "music-4"
+
+  },
+
+  {
+
+    name: "Love Story",
+
+    artist: "Taylor Swift",
+
+    img: "music-5",
+
+    src: "music-5"
+
+  },
+
+  {
+
+    name: "Menemukanmu",
+
+    artist: "Seventeen",
+
+    img: "music-6",
+
+    src: "music-6"
+
+  },
+
+  {
+
+    name: "Rewrite The Stars",
+
+    artist: "James Arthur,Anne-Marry",
+
+    img: "music-7",
+
+    src: "music-7"
+
+  },
+
+  {
+
+    name: "Jodoh Pasti Bertemu",
+
+    artist: "Afgan",
+
+    img: "music-8",
+
+    src: "music-8"
+
+  },
+
+  {
+
+    name: "Perahu Kertas",
+
+    artist: "Maudy Ayunda",
+
+    img: "music-9",
+
+    src: "music-9"
+
+  },
+
+  {
+
+    name: "Saturn",
+
+    artist: "SZA",
+
+    img: "music-10",
+
+    src: "music-10"
+
+},
+  
+  {
+
+    name: "Raspodi",
+
+    artist: "JKT48",
+
+    img: "music-11",
+
+    src: "music-11"
+
+  },
+
+  {
+
+    name: "Masih Hatiku",
+
+    artist: "Arsy Widianto,Tiara Andini",
+
+    img: "music-12",
+
+    src: "music-12"
+
+  },
+
+  {
+
+    name: "Jurus Rahasia Teleport",
+
+    artist: "JKT48",
+
+    img: "music-13",
+
+    src: "music-13"
+
+  },
+
+  {
+
+    name: "Semoga",
+
+    artist: "Maliq & D'Essentials",
+
+    img: "music-14",
+
+    src: "music-14"
+
+  },
+
+  {
+
+    name: "Semua Aku Dirayakan",
+
+    artist: "Nadin Amizah",
+
+    img: "music-15",
+
+    src: "music-15"
+
+  },
+
+  {
+
+    name: "All I Want",
+
+    artist: "Olivia Rodrigo,Disney",
+
+    img: "music-16",
+
+    src: "music-16"
+
+  },
+
+  {
+
+    name: "Bertaut",
+
+    artist: "Nadin Amizah",
+
+    img: "music-17",
+
+    src: "music-17"
+
+  },
+
+  {
+
+    name: "Dandelions",
+
+    artist: "Ruth b.",
+
+    img: "music-18",
+
+    src: "music-18"
+
+  },
+
+  {
+
+    name: "Akulah Dia",
+
+    artist: "Drive",
+
+    img: "music-19",
+
+    src: "music-19"
+
+  },
+
+  {
+
+    name: "Mesra'annya Kecil'lan Dulu",
+
+    artist: "Nadin Amizah",
+
+    img: "music-20",
+
+    src: "music-20"
+
+  },
+
+  {
+
+    name: "Cinta Jangan Sembunyi",
+
+    artist: "Seventeen",
+
+    img: "music-21",
+
+    src: "music-21"
+
+  },
+
+  {
+
+    name: "Separuhku",
+
+    artist: "Noah",
+
+    img: "music-22",
+
+    src: "music-22"
+
+  },
+
+  {
+
+    name: "Kangen",
+
+    artist: "Dewa 19",
+
+    img: "music-23",
+
+    src: "music-23"
+
+  },
+
+  {
+
+    name: "don't faze me",
+
+    artist: "axaero",
+
+    img: "music-24",
+
+    src: "music-24"
+
+  },
+
+  {
+
+    name: "Kembali Pulang",
+
+    artist: "Feby Putri",
+
+    img: "music-25",
+
+    src: "music-25"
+
+  },
+
+  {
+
+    name: "DITINGGAL BANG DIKA",
+
+    artist: "Dika",
+
+    img: "music-26",
+
+    src: "music-26"
+
+  },
+
+  {
+
+    name: "Aku,Kamu Dan Samudra",
+
+    artist: "Rebellion Rose",
+
+    img: "music-27",
+
+    src: "music-27"
+
+      },
+
+  {
+
+    name: "deja vu",
+
+    artist: "Olivia Rodrigo",
+
+    img: "music-28-29",
+
+    src: "music-28"
+
+  },
+
+  {
+
+    name: "happier",
+
+    artist: "Olivia Rodrigo",
+
+    img: "music-28-29",
+
+    src: "music-29"
+
+  },
+
+  {
+
+    name: "SNAP",
+
+    artist: "Rosa Linn",
+
+    img: "music-30",
+
+    src: "music-30"
+
+  },
+
+  {
+
+    name: "cardigan",
+
+    artist: "Taylor Swift",
+
+    img: "music-31",
+
+    src: "music-31"
+
+  },
+
+    {
+
+    name: "Sempurna",
+
+    artist: "Andra & The Backbone",
+
+    img: "music-32",
+
+    src: "music-32"
+
+    },
+
+  {
+
+    name: "Those Eyes",
+
+    artist: "New West",
+
+    img: "music-33",
+
+    src: "music-33"
+
+  },
+
+  {
+
+    name: "Lonely",
+
+    artist: "Akon",
+
+    img: "music-34",
+
+    src: "music-34"
+
+},
+
+  {
+
+    name: "Fearless",
+
+    artist: "Taylor Swift",
+
+    img: "music-35",
+
+    src: "music-35"
+
+    },
+
+  {
+
+    name: "SOS",
+
+    artist: "Seventeen",
+
+    img: "music-36",
+
+    src: "music-36"
+
+  },
+
+  {
+
+    name: "Together",
+
+    artist: "Seventeen",
+
+    img: "music-37",
+
+    src: "music-37"
+
+  },
+
+  {
+
+    name: "Kidult",
+
+    artist: "Seventeen",
+
+    img: "music-38",
+
+    src: "music-38"
+
+  },
+
+  {
+
+    name: "Wide Awake",
+
+    artist: "Katy Perry",
+
+    img: "music-39",
+
+    src: "music-39"
+
+  },
+
+  {
+
+    name: "The Lazy Song",
+
+    artist: "Bruno Mars",
+
+    img: "music-40",
+
+    src: "music-40"
+
+  },
+
+  {
+
+    name: "Someone To Stay",
+
+    artist: "Vancouver Sleep Clinic",
+
+    img: "music-41",
+
+    src: "music-41"
+
+  },
+
+  {
+
+    name: "Rasah Bali",
+
+    artist: "Lavaro,Ena Vika",
+
+    img: "music-42",
+
+    src: "music-42"
+
+  },
+
+  {
+
+    name: "Malampagi",
+
+    artist: "Saixse",
+
+    img: "music-43",
+
+    src: "music-43"
+
+  },
+
+  {
+
+    name: "Nemen",
+
+    artist: "Guyon Waton",
+
+    img: "music-44",
+
+    src: "music-44"
+
+  },
+
+  {
+
+    name: "Dumes",
+
+    artist: "OMWAWES,Guyon Waton",
+
+    img: "music-45",
+
+    src: "music-45"
+
+  },
+
+  {
+
+    name: "Jaga Selalu Hatimu",
+
+    artist: "Seventeen",
+
+    img: "music-46",
+
+    src: "music-46"
+
+  },
+
+  {
+
+    name: "Bersepeda Berdua",
+
+    artist: "JKT48",
+
+    img: "music-47",
+
+    src: "music-47"
+
+  },
+
+  {
+
+    name: "Dibuai",
+
+    artist: "The Paps",
+
+    img: "music-48",
+
+    src: "music-48"
+
+  },
+
+  {
+
+    name: "Titik Ketinggian",
+
+    artist: "BAXLAXBOY",
+
+    img: "music-49",
+
+    src: "music-49"
+
+  },
+
+  {
+
+    name: "Yang Terbaik Bagimu",
+
+    artist: "Donnie Sibarani,Aura",
+
+    img: "music-50",
+
+    src: "music-50"
+
+  },
+
+  {
+
+    name: "Mimpi Yang Sempurna",
+
+    artist: "Michela Thea",
+
+    img: "music-51",
+
+    src: "music-51"
+
+  },
+
+  {
+
+    name: "Say Yes To Heaven",
+
+    artist: "Lana Del Rey",
+    
+    img: "music-52",
+
+    src: "music-52"
+
+  }
+  
+
+];
